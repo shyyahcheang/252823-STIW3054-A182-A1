ab8e9c5bceca16a3a5df1a821296eace3c2229ab
shyyahcheang <47626271+shyyahcheang@users.noreply.github.com>
2019-03-09 19:05:26 +0800

    Update ActorType.java

--- src/assignment1/ActorType.java
+++ src/assignment1/ActorType.java
@@ -19,62 +19,62 @@ public class ActorType {
 	public void weightingActors() {
 		// TODO Auto-generated method stub
 		int qtySimpleAT = 0, qtyAverageAT = 0, qtyComplexAT = 0;
-        int totalSimpleAT, totalAverageAT, totalComplexAT;
-        @SuppressWarnings("resource")
+        	int totalSimpleAT, totalAverageAT, totalComplexAT;
+        	@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-        boolean repeat = true;
+        	boolean repeat = true;
 
-        System.out.println("Weighting Actors for Complexity");
-        System.out.println("===============================");
-        System.out.print("Simple - Defined API : ");
-        while (repeat) {
-            try {
-                qtySimpleAT = scan.nextInt();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input! Please key in an integer number.");
-                System.out.print("Simple - Defined API : ");
-                scan.next();
-                repeat = true;
-            }
-        }
+        	System.out.println("Weighting Actors for Complexity");
+        	System.out.println("===============================");
+        	System.out.print("Simple - Defined API : ");
+        	while (repeat) {
+            		try {
+                		qtySimpleAT = scan.nextInt();
+                		break;
+            		} catch (InputMismatchException e) {
+                		System.out.println("Invalid input! Please key in an integer number.");
+                		System.out.print("Simple - Defined API : ");
+                		scan.next();
+                		repeat = true;
+            		}
+        	}
 
-        System.out.print("Average - Interactive or Protocol driven interface : ");
-        while (repeat) {
-            try {
-                qtyAverageAT = scan.nextInt();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input! Please key in an integer number.");
-                System.out.print("Average - Interactive or Protocol driven interface : ");
-                scan.next();
-                repeat = true;
-            }
-        }
+        	System.out.print("Average - Interactive or Protocol driven interface : ");
+        	while (repeat) {
+            		try {
+                		qtyAverageAT = scan.nextInt();
+                		break;
+            		} catch (InputMismatchException e) {
+				System.out.println("Invalid input! Please key in an integer number.");
+				System.out.print("Average - Interactive or Protocol driven interface : ");
+				scan.next();
+				repeat = true;
+            		}
+        	}
 
-        System.out.print("Complex - Graphical User Interface : ");
-        while (repeat) {
-            try {
-                qtyComplexAT = scan.nextInt();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input! Please key in an integer number.");
-                System.out.print("Complex - Graphical User Interface : ");
-                scan.next();
-                repeat = true;
-            }
-        }
+		System.out.print("Complex - Graphical User Interface : ");
+		while (repeat) {
+		    try {
+			qtyComplexAT = scan.nextInt();
+			break;
+		    } catch (InputMismatchException e) {
+			System.out.println("Invalid input! Please key in an integer number.");
+			System.out.print("Complex - Graphical User Interface : ");
+			scan.next();
+			repeat = true;
+		    }
+		}
 
-        totalSimpleAT = qtySimpleAT*1;
-        totalAverageAT = qtyAverageAT*2;
-        totalComplexAT = qtyComplexAT*3;
-        totalAT = totalSimpleAT + totalAverageAT + totalComplexAT;
+		totalSimpleAT = qtySimpleAT*1;
+		totalAverageAT = qtyAverageAT*2;
+		totalComplexAT = qtyComplexAT*3;
+		totalAT = totalSimpleAT + totalAverageAT + totalComplexAT;
 
-        System.out.println("\nSub total for simple actor is " + totalSimpleAT);
-        System.out.println("Sub total for average actor is " + totalAverageAT);
-        System.out.println("Sub total for complex actor is " + totalComplexAT);
-        System.out.println("--------------------------------");
-        System.out.println("Total Actor Points are " + totalAT);
-        System.out.println("--------------------------------");
-	}
+		System.out.println("\nSub total for simple actor is " + totalSimpleAT);
+		System.out.println("Sub total for average actor is " + totalAverageAT);
+		System.out.println("Sub total for complex actor is " + totalComplexAT);
+		System.out.println("--------------------------------");
+		System.out.println("Total Actor Points are " + totalAT);
+		System.out.println("--------------------------------");
+		}
 }
