48f4bcb9f7c68547f96489b7635800929eb2ad62
shyyahcheang <47626271+shyyahcheang@users.noreply.github.com>
2019-03-09 19:06:51 +0800

    Update AdjustedManHours.java

--- src/assignment1/AdjustedManHours.java
+++ src/assignment1/AdjustedManHours.java
@@ -16,31 +16,31 @@ public class AdjustedManHours extends ManHours{
 		this.adjustedManHours = adjustedManHours;
 	}
 
-    public void adjustingManHours() {
-    	// TODO Auto-generated method stub
-        @SuppressWarnings("resource")
-        Scanner scan = new Scanner(System.in);
-        boolean repeat = true;
-        double rcp = 0.0;
+    	public void adjustingManHours() {
+    		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		boolean repeat = true;
+		double rcp = 0.0;
 
-        System.out.println("\nAdjusting Man-hours for Risk");
-        System.out.println("==============================");
-        System.out.print("Please key in the risk coefficient percentage : ");
-        while (repeat) {
-            try {
-                rcp = scan.nextDouble();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input! Please key in number.");
-                System.out.print("Please key in the risk coefficient percentage : ");
-                scan.next();
-                repeat = true;
-            }
-        }
+		System.out.println("\nAdjusting Man-hours for Risk");
+		System.out.println("==============================");
+		System.out.print("Please key in the risk coefficient percentage : ");
+		while (repeat) {
+		    try {
+			rcp = scan.nextDouble();
+			break;
+		    } catch (InputMismatchException e) {
+			System.out.println("Invalid input! Please key in number.");
+			System.out.print("Please key in the risk coefficient percentage : ");
+			scan.next();
+			repeat = true;
+		    }
+		}
 
-        adjustedManHours = (1.0 + rcp) * ManHours.manHours;
-        System.out.println("\n---------------------------------------");
-        System.out.printf("Adjusted Man-Hours are %.2f" , adjustedManHours);
-        System.out.println("\n---------------------------------------");
-    }
+		adjustedManHours = (1.0 + rcp) * ManHours.manHours;
+		System.out.println("\n---------------------------------------");
+		System.out.printf("Adjusted Man-Hours are %.2f" , adjustedManHours);
+		System.out.println("\n---------------------------------------");
+	}
 }
